61c39b26149b02a2f6d75740e279070310683981
ZoricaM <zo.mijatovic@gmail.com>
2018-06-05 00:17:51 +0200

    Novi Car

---
+++ JavaApplication1/src/javaMain.java
@@ -0,0 +1,25 @@
+
+public class Mani {
+
+    public static void main(String[] args) {
+        
+        Car myCar = new Car();
+        System.out.println("Maksimalna brzina je: " + myCar.maxSpeed);
+        System.out.ptintln(MyCar.condition);
+        System.out.ptintln(MyCar.currentPrice);
+        System.out.ptintln(MyCar.licence);
+        System.out.ptintln(MyCar.minSpeed);
+        System.out.ptintln(MyCar.weight);
+        System.out.ptintln(MyCar.licence);
+       
+        
+        Car companyCar = new Car(150, 30, 2000, false, "EF-123", 'B', 6000, 3500);
+        System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed);
+        
+    }
+    
+}
+
+
+        
+    
